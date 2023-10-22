920ffe9f8c89ed611a98e1ca08e2fa0da46f8993
Vince McKeown <vmckeown79@gmail.com>
2023-10-22 14:58:45 -0400

    Added second bookshelf

--- js/utilities/ImageLoading.js
+++ js/utilities/ImageLoading.js
@@ -63,6 +63,7 @@ function loadImages() {
 			{trackType: TILE_FINISH, theFile: "track_finish.png"},
 			{trackType: TILE_STAIRS, theFile: "track_stairs.png"},
 			{trackType: TILE_BOOKSHELF, theFile: "track_bookshelf.png"},
+			{trackType: TILE_BOOKSHELF_2, theFile: "bookshelf2.png"},
 			{trackType: TILE_FIRE_PLACE_LIT, theFile: "track_fireplace.png"},
 			{trackType: TILE_FIRE_PLACE, theFile: "track_fireplaceNotLit.png"},
 			{trackType: TILE_PITTRAP_ARMED, theFile: "track_road.png"},
